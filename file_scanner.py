88c4af8cf657de35d5b5516eaa153ff4ed1c8aec
wesamkiki <wesamkiki97@gmail.com>
2026-05-05 09:34:53 +0400

    Scanner folder V1

--- file_scanner.py
+++ file_scanner.py
@@ -1,1 +1,10 @@
 """Scan the Folder and list the file then send it one by one to classifier and resort it """
+from pathlib import Path
+from classifier import classify_file
+
+tempDB = {}
+def scanner(folder_path:str): 
+    folder = Path(folder_path)
+    for file in folder.iterdir():
+        
+        tempDB[file.name] = classify_file(Path(file))

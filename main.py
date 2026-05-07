8ed45b82477bc4d8ed2e284480fe0501fb1c7399
wesamkiki <wesamkiki97@gmail.com>
2026-05-07 18:26:37 +0400

    print_preview V1

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 from config import SOURCE_FOLDER 
 from planner import build_plan
 from file_counter import count_files_by_category
+from reporter import print_preview
 
 plan = build_plan(SOURCE_FOLDER)
 print(plan)
 
 summary = count_files_by_category(plan)
 print(summary)
+
+print_preview(plan,summary)
--- reporter.py
+++ reporter.py
@@ -1,1 +1,11 @@
 """Generator "Report" to show the files in the folder"""
+def print_preview(plan,summary):
+    print(f"""Smart Folder Organizer Preview\n""")
+    for category, files in plan.items():
+        print(f"{category} ({len(files)} files):")
+        for file in files:
+            print(f"- {file}")
+        print()
+    print(f"Summary:")
+    for category, file_num in summary.items():
+        print(f"{category}: {file_num}")

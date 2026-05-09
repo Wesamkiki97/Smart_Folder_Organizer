653b1fec8b5572245e40f3c6d6478b85b7afd184
wesamkiki <wesamkiki97@gmail.com>
2026-05-09 18:11:52 +0400

    Print Full Preview

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from config import SOURCE_FOLDER 
 from planner import build_plan
 from file_counter import count_files_by_category
-from reporter import print_preview
+from reporter import print_full_preview
 from mover import create_category_folders, preview_moves 
 
 
@@ -14,8 +14,12 @@ folder_summary = create_category_folders(SOURCE_FOLDER , plan)
 moves_preview = preview_moves(SOURCE_FOLDER,plan)
 
 
-print_preview(plan,summary)
-print("-----------------------------------------")
-print(folder_summary)
-print("-----------------------------------------")
-print(moves_preview)
+
+
+print_full_preview(
+    plan,
+    summary,
+    folder_summary,
+    moves_preview
+    )
+# print(moves_preview)
--- reporter.py
+++ reporter.py
@@ -4,8 +4,35 @@ def print_preview(plan,summary):
     for category, files in plan.items():
         print(f"{category} ({len(files)} files):")
         for file in files:
-            print(f"- {file}")
+            print(f"- {file.name}")
         print()
     print(f"Summary:")
     for category, file_num in summary.items():
         print(f"{category}: {file_num}")
+
+
+def print_full_preview(plan,summary,folder_summary,moves_preview):
+    print(f"""Smart Folder Organizer Preview\n""")
+    print("Scan source folder preview:")
+    for category, files in plan.items():
+        print(f"{category} ({len(files)} files):")
+        for file in files:
+            print(f"- {file.name}")
+        print()
+    print("---------------------------------")
+    print("Summary of files:")
+    for category, file_num in summary.items():
+        print(f"{category}: {file_num}")
+    print("---------------------------------")
+    print("folder execution:")
+    for status, folders in folder_summary.items():
+        print(f"{status}:")
+        for folder in folders:
+            print(f"-{folder}")
+        print()
+    print("---------------------------------")
+    print("moves files summary:")
+    for transaction in moves_preview:
+        print(".......................")
+        for key, value in transaction.items():
+            print(f"{key}: {value}")
